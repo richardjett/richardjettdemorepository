df9ff24724a18a3f44a1aeb2da61c9bb24ccd30f
Richard Jett <richardjett1@gmail.com>
2014-11-03 13:32:57 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>SMART Digital Marketing Soltuions</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+<table style="width: 100%;
+height: 100%;">
+<tr height="100%" valign="center"><td width="100%" align="center"><img src="images/sdms3.jpg" style="max-width:450px;"></td></tr></table>
+</body>
+</html>
